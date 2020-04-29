4edc51c44643d11529d5156529c22c4a96ad5152
Kalinss <Kalinss16@gmail.com>
2020-04-29 22:09:55 +0400

    update config, added favicon update header, added link for medium

--- project/src/components/header.js
+++ project/src/components/header.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faRss} from "@fortawesome/free-solid-svg-icons";
-import {faMedium, faGithub,faVk,faCodepen} from "@fortawesome/free-brands-svg-icons";
+// import {faRss} from "@fortawesome/free-solid-svg-icons";
+import {faMedium, faGithub,faVk} from "@fortawesome/free-brands-svg-icons";
 import {Link} from 'gatsby';
 
 const getActiveLink = (name) => window.location.pathname === name ? 'active':'';
 
 const Header = (props)=>{
-    console.log(window.location.pathname);
     return(
         <header className='header'>
-            <h1>Kalinss</h1>
+            <h1>Kalinin Sergey</h1>
             <nav>
                 <ul className="left">
                     <li className={getActiveLink('/')}> <Link to="/">home</Link></li>
@@ -18,24 +17,26 @@ const Header = (props)=>{
                 </ul>
                 <ul className="right">
                     <li>
-                        <FontAwesomeIcon className='icon'  icon={faMedium}/>
+                        <a href="https://medium.com/@kalinss">
+                            <FontAwesomeIcon className='icon'  icon={faMedium}/>
+                        </a>
                     </li>
                     <li>
-                        <a href="https://github.com/Kalinss" target="_blank">
+                        <a href="https://github.com/Kalinss">
                             <FontAwesomeIcon className='icon'   icon={faGithub}/>
                         </a>
                     </li>
                     <li>
-                        <a href="https://vk.com/hidashko" target="_blank">
+                        <a href="https://vk.com/hidashko">
                             <FontAwesomeIcon className='icon'   icon={faVk}/>
                         </a>
                     </li>
                     {/*<li>*/}
                     {/*    <FontAwesomeIcon className='icon'   icon={faCodepen}/>*/}
                     {/*</li>*/}
-                    <li>
-                        <FontAwesomeIcon className='icon'   icon={faRss}/>
-                    </li>
+                    {/*<li>*/}
+                    {/*    <FontAwesomeIcon className='icon'   icon={faRss}/>*/}
+                    {/*</li>*/}
 
                 </ul>
             </nav>
